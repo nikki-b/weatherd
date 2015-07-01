d4a030e903e1e204dffb2074a6b50d1f2e23c734
nikki-b <nikki.m.brown@gmail.com>
2015-07-01 15:23:23 -0700

    img fix

--- js/weatherd.js
+++ js/weatherd.js
@@ -17,27 +17,27 @@ $(document).ready(function(){
 			$("#today").append(
 				"<p>"+Math.round(output.list[0].temp.day)+"&deg;</p>"+
 				"<p>"+output.list[0].weather[0].description+
-				"</p><img src='/img/"+output.list[0].weather[0].icon+".svg'>"
+				"</p><img src='img/"+output.list[0].weather[0].icon+".svg'>"
 			);
 			$("#1").append(
 				"<p>"+Math.round(output.list[1].temp.day)+"&deg;</p>"+
 				"<p>"+output.list[1].weather[0].description+"</p>"+
-				"<img src='/img/"+output.list[1].weather[0].icon+".svg'>"
+				"<img src='img/"+output.list[1].weather[0].icon+".svg'>"
 			);
 			$("#2").append(
 				"<p>"+Math.round(output.list[2].temp.day)+"</p>&deg;"+
 				"<p>"+output.list[2].weather[0].description+"</p>"+
-				"<img src='/img/"+output.list[2].weather[0].icon+".svg'>"
+				"<img src='img/"+output.list[2].weather[0].icon+".svg'>"
 			);
 			$("#3").append(
 				"<p>"+Math.round(output.list[3].temp.day)+"</p>&deg;"+
 				"<p>"+output.list[3].weather[0].description+"</p>"+
-				"<img src='/img/"+output.list[3].weather[0].icon+".svg'>"
+				"<img src='img/"+output.list[3].weather[0].icon+".svg'>"
 			);
 			$("#4").append(
 				"<p>"+Math.round(output.list[4].temp.day)+"</p>&deg;"+
 				"<p>"+output.list[4].weather[0].description+"</p>"+
-				"<img src='/img/"+output.list[4].weather[0].icon+".svg'>"
+				"<img src='img/"+output.list[4].weather[0].icon+".svg'>"
 			);
 			$("#location").text(output.city.name);
 			console.log("Weather ID", output.list[0].weather[0].icon);
